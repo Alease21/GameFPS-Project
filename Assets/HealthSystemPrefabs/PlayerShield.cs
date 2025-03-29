281d2218e1991b9942f3e311adc0b89b183f84c4
Brrobinson03 <148285717+Brrobinson03@users.noreply.github.com>
2025-03-28 22:18:37 -0500

    Add files via upload

---
+++ Assets/HealthSystemPrefabs/PlayerShield.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerShield : MonoBehaviour
+{
+    public int _currShield;
+    public int _maxShield = 50;
+    public TMP_Text ShieldBarValueText;
+
+    void Start()
+    {
+        _currShield = _maxShield;
+    }
+
+    private void FixedUpdate()
+    {
+        ShieldBarValueText.text = _currShield.ToString() + "/" + _maxShield.ToString();
+    }
+
+    // Start is called before the first frame update
+
+    public void TakeDamage(int damage)
+    {
+        _currShield -= damage;
+    }
+}
