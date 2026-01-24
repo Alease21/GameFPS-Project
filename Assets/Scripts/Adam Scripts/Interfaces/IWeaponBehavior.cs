f227ccc04e0d7ab58e9709e6013913fd92a29ea9
Alease21 <alease21@outlook.com>
2026-01-24 04:03:22 -0600

    Purge

--- Assets/Scripts/Adam Scripts/Interfaces/IWeaponBehavior.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IWeaponBehavior
-{
-    void FireGun(Transform shootPoint);
-}
--- Assets/Scripts/Adam Scripts/Interfaces/iRechargeableItem.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface iRechargeableItem
-{
-    public void RechargeItem(float rechargeTime);
-}
